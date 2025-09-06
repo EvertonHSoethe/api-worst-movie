e1f975ddfde32ba79989c5f6197d8ecb922f364b
Everton Henrique Soethe <evertonhsoethe@gmail.com>
2025-09-06 20:53:01 -0300

    feat: Initial commit

---
+++ src/main/java/com/outsera/api_worst_movie/ApiWorstMovieApplication.java
@@ -0,0 +1,13 @@
+package com.outsera.api_worst_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWorstMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWorstMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outsera/api_worst_movie/ApiWorstMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outsera.api_worst_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWorstMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
